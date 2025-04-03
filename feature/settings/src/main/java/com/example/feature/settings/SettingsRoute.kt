3e003356979e694922003a62f6b4c2349353abf2
Michauu035 <51509107+Michauu035@users.noreply.github.com>
2025-04-03 17:04:18 +0200

    feat: add Settings destination and route for navigation

---
+++ feature/settings/src/main/java/com/example/feature/settings/SettingsRoute.kt
@@ -0,0 +1,29 @@
+package com.example.feature.settings
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.example.feature.uicore.navigation.AppIcon
+import com.example.feature.uicore.navigation.Destination
+import kotlinx.serialization.Serializable
+import org.koin.androidx.compose.koinViewModel
+
+@Serializable
+object Settings : Destination(
+	"Settings",
+	AppIcon.VectorIcon(Icons.Default.Settings, "Settings icon"),
+)
+
+fun NavGraphBuilder.settingsRoute(openDrawer: () -> Unit) {
+	composable<Settings> {
+		val viewModel = koinViewModel<SettingsViewModel>()
+		SettingsScreen(
+			openDrawer = openDrawer,
+			viewModel = viewModel,
+			modifier = Modifier.fillMaxSize(),
+		)
+	}
+}
