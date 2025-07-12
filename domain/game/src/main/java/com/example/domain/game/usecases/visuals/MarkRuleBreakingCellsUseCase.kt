2591b65ee906468880f0b2c4cc17d14f1c98afb0
Michauu035 <51509107+Michauu035@users.noreply.github.com>
2025-07-12 21:36:21 +0200

    Refactor: replace firstOrNull with first in HighlightMatchingNumbersUseCase and MarkRuleBreakingCellsUseCase

--- domain/game/src/main/java/com/example/domain/game/usecases/visuals/MarkRuleBreakingCellsUseCase.kt
+++ domain/game/src/main/java/com/example/domain/game/usecases/visuals/MarkRuleBreakingCellsUseCase.kt
@@ -3,7 +3,7 @@ package com.example.domain.game.usecases.visuals
 import com.example.domain.settings.SettingsRepository
 import com.example.sudoku.model.SudokuGrid
 import com.example.sudoku.model.markRuleBreakingCells
-import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.first
 
 class MarkRuleBreakingCellsUseCase(
 	private val settingsRepository: SettingsRepository,
@@ -11,7 +11,7 @@ class MarkRuleBreakingCellsUseCase(
 ) {
 	suspend operator fun invoke(sudokuGrid: SudokuGrid): SudokuGrid {
 		val clearedGrid = clearRuleBreakingCellsUseCase(sudokuGrid)
-		val highlightInvalid = settingsRepository.highlightInvalidNumbers.firstOrNull() ?: true
+		val highlightInvalid = settingsRepository.highlightInvalidNumbers.first()
 		return if (highlightInvalid) {
 			clearedGrid.markRuleBreakingCells()
 		} else {
