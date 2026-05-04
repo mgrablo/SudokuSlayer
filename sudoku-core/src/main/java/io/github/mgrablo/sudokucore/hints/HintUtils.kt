222bc17f4780212cf781313fde6a30aa6021f993
mgrablo <51509107+mgrablo@users.noreply.github.com>
2026-05-04 16:43:59 +0200

    Refactor: Rename hint utility functions for clarity

--- sudoku-core/src/main/java/io/github/mgrablo/sudokucore/hints/HintUtils.kt
+++ sudoku-core/src/main/java/io/github/mgrablo/sudokucore/hints/HintUtils.kt
@@ -3,13 +3,17 @@ package io.github.mgrablo.sudokucore.hints
 import io.github.mgrablo.sudokucore.model.SudokuCellData
 import kotlin.math.sqrt
 
-internal fun getRow(data: List<SudokuCellData>, row: Int): List<SudokuCellData> =
+internal fun getRowCells(data: List<SudokuCellData>, row: Int): List<SudokuCellData> =
 	data.filter { it.row == row }
 
-internal fun getColumn(data: List<SudokuCellData>, col: Int): List<SudokuCellData> =
+internal fun getColumnCells(data: List<SudokuCellData>, col: Int): List<SudokuCellData> =
 	data.filter { it.col == col }
 
-internal fun getBlock(data: List<SudokuCellData>, boxRow: Int, boxCol: Int): List<SudokuCellData> {
+internal fun getBlockCells(
+	data: List<SudokuCellData>,
+	boxRow: Int,
+	boxCol: Int,
+): List<SudokuCellData> {
 	val gridSize = sqrt(data.size.toDouble()).toInt()
 	val blockSize = sqrt(gridSize.toDouble()).toInt()
 	val startRow = boxRow * blockSize
@@ -19,6 +23,9 @@ internal fun getBlock(data: List<SudokuCellData>, boxRow: Int, boxCol: Int): Lis
 			it.col in startCol until startCol + blockSize
 	}
 }
+
+internal fun getBlockId(row: Int, col: Int, blockSize: Int): Int =
+	(row / blockSize) * blockSize + (col / blockSize)
 
 internal fun Collection<SudokuCellData>.containsCell(cell: SudokuCellData): Boolean =
 	this.any { it.row == cell.row && it.col == cell.col }
